c0206a039543da6a3c3b9505b1952b1844abc5ba
andreshapradana <andresha.pradana@gmail.com>
2022-09-21 14:25:54 +0700

    edit html

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,11 +6,25 @@ from django.core import serializers
 # Create your views here.
 def show_mywatchlist(request):
     data_mywatchlist = MyWatchList.objects.all()
+    counteryes = 0
+    counterno = 0
+    for movie in data_mywatchlist:
+        if (movie.watched == "Yes"):
+            counteryes += 1
+        elif (movie.watched == "No"):
+            counterno += 1
+    if (counteryes > counterno):
+        message = "Selamat, kamu sudah banyak menonton!"
+    else:
+        message = "Wah, kamu masih sedikit menonton!"
+        
     context = {
         'list_movie': data_mywatchlist,
         'nama': 'Andresha Pradana', 
-        'NPM' : '2106651591'
+        'NPM' : '2106651591',
+        'Pesan' : message
     }
+        
     return render(request, "mywatchlist.html", context)
 
 def show_xml(request):
